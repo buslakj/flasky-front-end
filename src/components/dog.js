bf1d1b1b4309a97f7083348b0eca111bd009ae64
Kassidy Buslach <kjbuslach@gmail.com>
2022-06-14 10:58:55 -0700

    added hardcoded dogs

---
+++ src/components/dog.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Dog = () => {
+  return (
+    <li>
+      <h3>A soon to be named dog</h3>
+      <span>chip #:</span>
+      <button>Add chip</button>
+    </li>
+  );
+};
+export default Dog;
